5e0eff88ef896492d0daaaf2edaa9b625d3f8789
Isabel Chambel <isabel.chambel@celfocus.com>
2022-07-04 15:09:26 +0100

    Fix Navbar icon function

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,34 +3,31 @@ import { DarkTheme, LightTheme } from "../../assets/svg";
 import "./_navbar.scss";
 
 const Navbar = () => {
-    const [isLightTheme, setLightTheme] = useState<boolean>(true);
+  const [isLightTheme, setLightTheme] = useState<boolean>(true);
 
-    useEffect(() => {
-        const htmlElement = document.documentElement;
+  useEffect(() => {
+    const htmlElement = document.documentElement;
 
-        const theme = isLightTheme ? "light" : "dark";
+    const theme = isLightTheme ? "light" : "dark";
 
-        htmlElement.setAttribute("data-theme", theme);
-    }, [isLightTheme]);
+    htmlElement.setAttribute("data-theme", theme);
+  }, [isLightTheme]);
 
-    const changeTheme = () => {
-        setLightTheme(!isLightTheme);
-    };
+  const changeTheme = () => {
+    setLightTheme(!isLightTheme);
+  };
 
-    let icon = (
-        <button className="background--none" onClick={changeTheme}>
-            <DarkTheme className="icon icon__fill--none" />
-        </button>
-    );
+  const resolveIcon = isLightTheme ? (
+    <button className="background--none" onClick={changeTheme}>
+      <DarkTheme className="icon icon__fill--none" />
+    </button>
+  ) : (
+    <button className="background--none" onClick={changeTheme}>
+      <LightTheme className="icon icon__fill--none icon__fill--background-primary" />
+    </button>
+  );
 
-    if (!isLightTheme) {
-        icon = (
-            <button className="background--none" onClick={changeTheme}>
-                <LightTheme className="icon icon__fill--none icon__fill--background-primary" />
-            </button>
-        );
-    }
-    return <nav className="navbar flex flex--end-X">{icon}</nav>;
+  return <nav className="navbar flex flex--end-X">{resolveIcon}</nav>;
 };
 
 export default Navbar;
