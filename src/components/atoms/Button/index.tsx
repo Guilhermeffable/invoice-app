cd55f52939ea5a3951a0fdcb616d034883cacf73
Guilherme Luís <guilherme.luis@celfocus.com>
2022-08-31 10:06:17 +0100

    Organizing folder structure

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,80 @@
+import React, { ChangeEventHandler, Fragment, MouseEventHandler } from "react";
+import PropTypes from "prop-types";
+import GenericButton from "./GenericButton/genericButton";
+import PillButton from "./PillButton";
+import { ButtonProps } from "./types";
+
+const Button = ({
+  buttonStyle,
+  type,
+  text,
+  name,
+  onClick,
+  onChange,
+  icon,
+  value,
+}: ButtonProps) => {
+  switch (buttonStyle) {
+    case "primary":
+      return (
+        <GenericButton
+          text={text!}
+          primary={true}
+          onClick={onClick!}
+          icon={icon}
+          type={type}
+          value={value}
+          name={name}
+        />
+      );
+
+    case "secondary":
+      return (
+        <GenericButton
+          text={text!}
+          primary={false}
+          onClick={onClick!}
+          icon={icon}
+          type={type}
+          value={value}
+          name={name}
+        />
+      );
+
+    case "inline":
+      return (
+        <GenericButton
+          text={text!}
+          primary={false}
+          onClick={onClick!}
+          icon={icon}
+          type={type}
+          value={value}
+          name={name}
+          inline={true}
+        />
+      );
+
+    case "pill":
+      return (
+        <PillButton
+          text={text!}
+          value={value!}
+          name={name}
+          onChange={onChange!}
+        />
+      );
+
+    default:
+      return <Fragment></Fragment>;
+  }
+};
+
+Button.propTypes = {
+  buttonStyle: PropTypes.string.isRequired,
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+  icon: PropTypes.object,
+};
+
+export default Button;
